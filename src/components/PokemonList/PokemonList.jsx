3b16d342477f71177c1047ac8d9c65186d663f0b
anddreluis2 <anddreluis98@gmail.com>
2023-04-05 00:50:15 -0300

    rendered pokemon names

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
+import { getPokemon } from '../../services/getPokemons'
+import { Link } from 'react-router-dom'
 
 export function PokemonList() {
+    const [pokemons, setPokemons] = useState([])
+    useEffect(() => {
+        const loadData = async () => {
+            const getPokemons = await getPokemon()
+            setPokemons(getPokemons)
+        }
+        loadData()
+    }, [])
+
+    console.log('teste', pokemons)
     return (
-        <>
-            <div className={styles.header}>
+        <div className={styles.wrapper}>
+            <div className={styles.title}>
                 <h1>Lista</h1>
             </div>
-            <div>teste</div>
-        </>
+            {pokemons?.map((pokemon) => {
+                return <div className={styles.pokemonName}>{pokemon?.name}</div>
+            })}
+        </div>
     )
 }
--- src/services/getPokemons.js
+++ src/services/getPokemons.js
@@ -3,7 +3,6 @@ import { api } from './api'
 export async function getPokemon(params) {
     if (params === undefined) {
         const response = await api.get(``)
-        console.log(response.data)
         return response.data.results
     }
 }
